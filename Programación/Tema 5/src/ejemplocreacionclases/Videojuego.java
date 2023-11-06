c0bb20802c245ed009aee526b42e544a40e0a9f3
Reo <jfg0010@alu.medac.es>
2023-11-06 20:53:31 +0100

    1

--- Programación/Tema 5/src/ejemplocreacionclases/Videojuego.java
+++ Programación/Tema 5/src/ejemplocreacionclases/Videojuego.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejemplocreacionclases;
 
-/**
- *
- * @author kekom
- */
 public class Videojuego {
        private int pegi;
        private double precio,nota;
