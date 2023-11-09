d90d4b84ba19722319529599c04491fa97b18041
AlejandroCastilloRamos <116159400+AlejandroCastilloRamos@users.noreply.github.com>
2023-11-09 19:59:06 +0100

    1

---
+++ Programación/Tema 6/src/Carniceria/Cerdo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Carniceria;
+
+/**
+ *
+ * @author kekom
+ */
+public class Cerdo {
+    private String origen,fechacaducidad;
+    private int porcentajegrasa,peso;
+    private double preciokilo;
+
+    
+   public Cerdo (){
+    origen ="";
+    fechacaducidad ="";
+    porcentajegrasa =0;
+    peso =0;
+    preciokilo=0;
+   } 
+    
+   
+}
---
+++ Programación/Tema 6/src/Carniceria/Ternera.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Carniceria;
+
+/**
+ *
+ * @author kekom
+ */
+public class Ternera {
+    private String origen,fechacaducidad;
+    private int porcentajegrasa,peso;
+    private double preciokilo;
+
+   public Ternera (){
+    origen ="";
+    fechacaducidad ="";
+    porcentajegrasa =0;
+    peso =0;
+    preciokilo=0;
+   }     
+}
