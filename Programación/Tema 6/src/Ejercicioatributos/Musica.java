d7b189ae9e317fa33c8289aa01d06649c5998d19
Reo <jfg0010@alu.medac.es>
2023-11-09 20:14:15 +0100

    1

--- Programación/Tema 6/src/Ejercicioatributos/Musica.java
+++ Programación/Tema 6/src/Ejercicioatributos/Musica.java
@@ -1,24 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Ejercicioatributos;
 
-/**
- *
- * @author kekom
- */
 public class Musica {
- private String genero,artista,fechaestreno;
-    private int pista,bpm;
-    private double preciocd;
-    
-    public Musica (){
-     genero="";
-    artista ="";
-    fechaestreno ="";
-    pista =0;
-    bpm =0;
-    preciocd=0;
-   }         
+   private String genero, artista, fechaestreno;
+   private int pista, bpm;
+   private double preciocd;
+
+   public Musica() {
+      genero = "";
+      artista = "";
+      fechaestreno = "";
+      pista = 0;
+      bpm = 0;
+      preciocd = 0;
+   }
+
+   public Musica(String genero, String artista, String fechaestreno, int pista, int bpm, double preciocd) {
+      this.genero = genero;
+      this.artista = artista;
+      this.fechaestreno = fechaestreno;
+      this.pista = pista;
+      this.bpm = bpm;
+      this.preciocd = preciocd;
+   }
+
+   public String Getogenero() {
+      return genero;
+   }
+
+   public void setgenero() {
+      this.genero = genero;
+   }
+
+   public String Getartista() {
+      return artista;
+   }
+
+   public void setartista() {
+      this.artista = artista;
+   }
+
+   public String Getfechaestreno() {
+      return fechaestreno;
+   }
+
+   public void Setfechaestreno() {
+      this.fechaestreno = fechaestreno;
+   }
+
+   public int Getpista() {
+      return pista;
+   }
+
+   public void setpista() {
+      this.pista = pista;
+   }
+
+   public double Getbpm() {
+      return bpm;
+   }
+
+   public void setbpm() {
+      this.bpm = bpm;
+   }
+
+
+   @Override
+   public String toString() {
+      return "Musica{" + "genero=" + genero + ", artista=" + artista
+            + ", fechaestreno=" + fechaestreno + ", pista="
+            + pista + ", bpm=" + bpm + '}';
+   }
+
 }
