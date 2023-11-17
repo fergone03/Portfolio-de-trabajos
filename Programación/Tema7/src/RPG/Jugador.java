720d8bf7c0652b15d91b660ea0a8a6d22a8aa9ce
Reo <jfg0010@alu.medac.es>
2023-11-17 19:28:36 +0100

    q

--- Programación/Tema7/src/RPG/Jugador.java
+++ Programación/Tema7/src/RPG/Jugador.java
@@ -1,13 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package RPG;
+
+import java.util.Random;
 
 /**
  *
  * @author kekom
  */
 public class Jugador {
-    
+    private String Nombre;
+    private int PuntosSalud;
+    private int PuntosAtaque;
+    private int Dinero;
+
+    public Jugador(String Nombre) {
+        this.Nombre = Nombre;
+        this.PuntosSalud = 20;
+        this.PuntosAtaque = 0;
+        this.Dinero = 2;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getPuntosSalud() {
+        return PuntosSalud;
+    }
+
+    public void setPuntosSalud(int puntosSalud) {
+        PuntosSalud = puntosSalud;
+    }
+
+    public int getPuntosAtaque() {
+        return PuntosAtaque;
+    }
+
+    public void setPuntosAtaque(int puntosAtaque) {
+        PuntosAtaque = puntosAtaque;
+    }
+
+    public int getDinero() {
+        return Dinero;
+    }
+
+    public void setDinero(int dinero) {
+        Dinero = dinero;
+    }
+
+    public void calcularFuerzaInicial() {
+        Random random = new Random();
+        this.puntosAtaque = random.nextInt(10) + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "nombre='" + nombre + '\'' +
+                ", puntosSalud=" + puntosSalud +
+                ", puntosAtaque=" + puntosAtaque +
+                ", dinero=" + dinero +
+                '}';
+
+    }
 }
