0d8fecf334f0be6ce641317c917e4bda3c34d330
Reo <jfg0010@alu.medac.es>
2024-04-10 14:56:54 +0200

    s

--- Programación/Tema 20/DigiPAImon/Main.py
+++ Programación/Tema 20/DigiPAImon/Main.py
@@ -1,13 +1,13 @@
 import random
-import Digipaimon
-import Enemigo
-import Jugador
-import ListaNombres
-import Inventario
+from Digipaimon import Digipaimon
+from Enemigo import Enemigo
+from Jugador import Jugador
+from ListaNombres import ListaNombres
+from Inventario import Inventario
 class Main:
     def generar_digipaimon_aleatorio(self):
         self.vida = random.randint(10,20)
         self.ataque = random.randint(1,10)
         self.nivel = random.randint(1,3)
         self.tipo  = random.choice(['fuego', 'agua','planta'])
-        self.nombre = random.choice(ListaNombres.lista_nombres_digipaimons)
+        self.nombre = ListaNombres.obtener_nombre_digipaimon()
