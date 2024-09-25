64ddd7ccb5c370fb33fff51fc5f82f7a142bc73b
fergone03 <114108805+fergone03@users.noreply.github.com>
2024-09-25 23:48:18 +0200

    1

--- Segundo/Interfaces/html/index.js
+++ Segundo/Interfaces/html/index.js
@@ -1,16 +1,15 @@
 document.getElementById("submitBtn").addEventListener("click", function () {
-  document.getElementById("myForm").style.display = "none";
-  document.getElementById("submitBtn").style.display = "none";
-  document.getElementById("myImage").style.display = "block";
-  document.getElementById("Bomboclat").innerHTML =
-    "¡Luffy se ha salvado gracias a tí!";
-});
-document.getElementById("submitBtn").addEventListener("click", function() {
-    var inputValue = document.getElementById("card").value;
+  var inputValue = document.getElementById("card").value;
 
-    if (inputValue.trim() === "") {
-        alert("Please fill out the input field.");
-    } else {
-        alert("Input submitted: " + inputValue);
-    }
+  if (inputValue.trim() === "") {
+    alert("Please fill out the input field.");
+  } else {
+    document.getElementById("submitBtn").addEventListener("click", function () {
+      document.getElementById("myForm").style.display = "none";
+      document.getElementById("submitBtn").style.display = "none";
+      document.getElementById("myImage").style.display = "block";
+      document.getElementById("Bomboclat").innerHTML =
+        "¡Luffy se ha salvado gracias a tí!";
+    });
+  }
 });
