349ff7525f45875e12118ac3e5ce3950e565fafd
Reo <fergonesteban03@gmail.com>
2024-09-25 20:03:05 +0200

    1

---
+++ Segundo/Servidor/Tema1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Cuadrado blanco</title>
+  <link rel="stylesheet" href="paginilla.css">
+</head>
+
+<body>
+<!-- Instrucción en PHP que escribe un hola mundo antes del container-->
+  <?php
+  echo "Hola Mundo!!!";
+  ?>
+    <div id="container">
+      <input type="text" class="xd1">
+      </div>
+</body>
+</html>
