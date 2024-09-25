c7567ad81bf8238a01f20cf93042dd2103fd24b6
fergone03 <114108805+fergone03@users.noreply.github.com>
2024-09-26 00:19:23 +0200

    1

--- Segundo/Interfaces/html/index.js
+++ Segundo/Interfaces/html/index.js
@@ -1,15 +1,23 @@
 document.getElementById("submitBtn").addEventListener("click", function () {
-  var inputValue = document.getElementById("card").value;
-
-  if (inputValue.trim() === "") {
-    alert("Please fill out the input field.");
-  } else {
-    document.getElementById("submitBtn").addEventListener("click", function () {
+    var cardValue = document.getElementById("card").value;
+    var nameValue = document.getElementById("name").value;
+    var yearsValue = document.getElementById("years").value;
+    var fechaValue = document.getElementById("fecha").value;
+  
+ 
+    if (
+      cardValue.trim() === "" ||
+      nameValue.trim() === "" ||
+      yearsValue.trim() === "" ||
+      fechaValue.trim() === ""
+    ) {
+      alert("Rellena todos los campos o Luffy pal lobby");
+    } else {
       document.getElementById("myForm").style.display = "none";
       document.getElementById("submitBtn").style.display = "none";
       document.getElementById("myImage").style.display = "block";
       document.getElementById("Bomboclat").innerHTML =
         "¡Luffy se ha salvado gracias a tí!";
-    });
-  }
-});
+    }
+  });
+  
