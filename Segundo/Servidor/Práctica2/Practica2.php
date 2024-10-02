ce70146151a202ce7b3dddd6e14263989c94653f
Reo <fergonesteban03@gmail.com>
2024-10-02 16:45:16 +0200

    "Updated Practica2.php: added HTML structure, modified echo statements, and added new variables and array operations."

--- Segundo/Servidor/Práctica2/Practica2.php
+++ Segundo/Servidor/Práctica2/Practica2.php
@@ -5,29 +5,42 @@
 </head>
 
 <body>
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
 <?php
 
-
+// Existing variables
 $saludo1 = "Hola";
 $saludo2 = "Mundo";
 $saludoCompleto = $saludo1 . " " . $saludo2;
 
 $esVerdadero = false;
 
-$pi = 3.14; 
-
+$pi = 3.14;
 
 $numeros = array("valor1" => 1,  "valor2" => 2, "valor3" => 3, );
-  
 
-  
-
-
-echo "Saludo: " . $saludoCompleto . "<br>";
-echo "Es verdadero: " . ($esVerdadero) . "<br>";
+echo "Saludo: " . str_replace(" ", "", $saludoCompleto) . "<br>";
+echo "Es verdadero: " . ($esVerdadero ? "true" : "false") . "<br>";
 echo "Valor de Pi: " . $pi . "<br>";
 echo "Valores del array: <br>";
-print_r($numeros); 
+print_r($numeros);
+echo "<br>";
+
+$longvar = "El operador “+” sirve para sumar el valor de variables. 
+Con la “/” podemos dividir valores entre variables. El símbolo del dólar 
+$ indica que estamos utilizando variables pero no lo usaremos en 
+las constantes o globales. (1 punto)<br>";
+echo "Tiene " . strlen($longvar) . " carácteres.<br>";
+
+$numeros2 = array(1.5, 2.6, 3.7, 4.8);
+echo "La posición 0 es " . $numeros2[0] . "<br>";
+
 
 ?>
 
