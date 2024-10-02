5e4c39f7db3e58a1d081e7ebb27d3530a5881990
Reo <fergonesteban03@gmail.com>
2024-10-02 20:45:56 +0200

    1

--- Segundo/Servidor/Práctica2/Ejercicios1a7.php
+++ Segundo/Servidor/Práctica2/Ejercicios1a7.php
@@ -39,18 +39,7 @@ $valor1 = $_GET["numero1"] ?? null;
 if ($valor1 == "" ) {
     $valor1 = "¡Escribe algo!";
 }
-//EJ8 pero se pone aquí porque si no no se muestra 
-if (is_int($valor1)) {
-    echo "El valor es un entero: " . $valor1;
-} elseif (is_string($valor1)) {
-    echo "El valor es una cadena de texto: " . $valor1;
-} elseif (is_bool($valor1)) {
-    echo "El valor es un booleano: " . ($valor1 ? 'true' : 'false');
-} else {
-    echo "El tipo de dato no es reconocido.";
-}
-//
-//URLhttp://localhost:3000/Pr%C3%A1ctica2/Practica2.php?numero1=casa
+
 echo 'Número 1= ' . $valor1 . "<br>";
 
 $potencia = $valor1 * $valor1;
