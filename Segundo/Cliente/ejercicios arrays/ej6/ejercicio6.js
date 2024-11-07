bd2ee38525dff132b74f64809067619862cf2434
Reo <fergonesteban03@gmail.com>
2024-11-07 21:48:29 +0100

    +ejercicios javi
    updated ej 6 y arreglado el uno de alex

--- Segundo/Cliente/ejercicios arrays/ej6/ejercicio6.js
+++ Segundo/Cliente/ejercicios arrays/ej6/ejercicio6.js
@@ -1,5 +1,4 @@
 let userWords = [];
-
 let coinciden = [];
 
 // Pedir al usuario que ingrese elementos hasta que el array tenga 8 elementos
@@ -7,7 +6,9 @@ for (let i = 0; i < 8; i++) {
   let input = prompt("Ingresa un elemento para el array:"); // Añadir el elemento ingresado al array
   userWords.push(input);
 }
-
+document.write(
+  "<p>Los elementos introducidos por el usario al principio son: " + userWords+"</p>"
+);
 let colors = [
   "red",
   "green",
@@ -18,7 +19,7 @@ let colors = [
   "black",
   "white",
 ];
-
+document.write("<p>Los colores que pueden ser coincidentes son: " + colors+"</p>");
 // Iterar sobre el primer array
 for (let i = 0; i < userWords.length; i++) {
   // Si el elemento del primer array está en el segundo array
@@ -30,6 +31,11 @@ for (let i = 0; i < userWords.length; i++) {
 
 userWords = userWords.filter((item) => !coinciden.includes(item));
 
+document.write("<p>Los elementos que coinciden son: " + coinciden+"</p>");
+
 userWords = coinciden.concat(userWords);
 
-console.log(userWords);
+document.write(
+  "<p>El array con los elementos coincidentes al principio de la lista es: " +
+    userWords+"</p>"
+);
